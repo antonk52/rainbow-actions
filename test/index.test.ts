cfe7f1cca92bb56ed327db1e1d44d0adb78b8c68
antonk52 <halloy52@gmail.com>
2025-04-04 20:39:49 +0100

    replace jest with vitest

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,4 @@
+import {describe, it, expect} from 'vitest';
 import {createActions} from '../src/index';
 
 describe('createActions', () => {
--- test/safe.test.ts
+++ test/safe.test.ts
@@ -1,3 +1,4 @@
+import {describe, it, expect} from 'vitest';
 import {createActions} from '../src/safe';
 
 describe('createActions', () => {
